e38c7f8ca6bccbc7d9a8f7b2f8cf9eab18f44c26
fengsq <11@123.com>
2022-06-10 11:17:52 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <Index/>
 </template>
+
+<script lang="ts">
+import {Options, Vue} from 'vue-class-component';
+import Index from '@/components/Index.vue'; // @ is an alias to /src
+
+@Options({
+  components: {
+    Index,
+  },
+})
+export default class HomeView extends Vue {
+}
+</script>
 
 <style>
 #app {
---
+++ src/components/Index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-aside width="200px">
+        <AsideBar/>
+      </el-aside>
+      <el-container>
+        <el-header>Header
+        </el-header>
+        <el-main>
+          <Temperature/>
+          <el-button @click="printHealth()">main</el-button>
+        </el-main>
+        <el-footer>footer
+        </el-footer>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import AsideBar from './AsideBar.vue'
+import Temperature from "@/components/temperature";
+import {healthCheck} from "@/request/baseapi";
+
+export default {
+  name: 'Index-Tab',
+  components: {
+    AsideBar,
+    Temperature
+  },
+  methods: {
+    printHealth() {
+      healthCheck().then(res => {
+        console.log(res.status);
+      }).catch(err => {
+        console.log(err);
+      });
+    }
+  },
+};
+</script>
+<style>
+</style>
