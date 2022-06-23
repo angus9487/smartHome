69896cd5ea0b67dd4dca1eee2f7df35b8bf54134
fengsq <11@123.com>
2022-06-23 17:29:36 +0800

    update

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,33 +1,35 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import {createRouter, createWebHistory, RouteRecordRaw} from 'vue-router'
+// import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
+import Floorplan from '../components/Floorplan.vue'
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import('../views/AboutView.vue')
-  },
-  {
-    path: '/overview',
-    name: 'overview',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import('../components/Floorplan.vue')
-  }
+    {
+        path: '/',
+        name: 'home',
+        component: Floorplan
+    },
+    {
+        path: '/about',
+        name: 'about',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: AboutView
+    },
+    {
+        path: '/overview',
+        name: 'overview',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: Floorplan
+    }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 export default router
